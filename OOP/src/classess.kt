8b0fcce38b320d71779c51d7d20b7f4c7b4c1f1b
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-08-07 22:27:18 +0545

    kotlin oop basic

---
+++ OOP/src/classess.kt
@@ -0,0 +1,10 @@
+class Student(val name: String,val age: Int){
+
+    fun greet(){
+        println("Student name is $name and $age years old..")
+    }
+}
+fun main(){
+    val person = Student("santosh",20)
+    person.greet();
+}
