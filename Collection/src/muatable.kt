a4108e0d5390e064046b34dfa7b7df3de54bb594
saugatgautam5363 <156888983+saugatgautam5363@users.noreply.github.com>
2025-08-07 18:37:58 +0545

    kotlin mutable collection

---
+++ Collection/src/muatable.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val names = mutableListOf("Saugat","Alisha","Biahal","Sandesh")
+    names.add("Sadiksha")
+    names.remove("Alisha")
+
+    for (i in names){
+        println(i)
+    }
+
+}
